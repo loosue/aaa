fab840beb577eb0dc881cd4c6cb0a1414945bf35
loosue <15870959327@163.com>
2017-04-18 10:13:08 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <my-header></my-header>
+    <hello></hello>
+    <my-footer></my-footer>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import myHeader from './components/Header'
+import myFooter from './components/Footer'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,myHeader,myFooter
+  }
+}
+</script>
+
+<style>
+html,body,header,footer,h1,h2,p,ul,li,div{padding: 0;margin: 0;}
+h1, h2 {font-weight: normal;}
+ul {list-style-type: none;}
+li {display: inline-block;margin: 0 10px;}
+a {color: #42b983;}
+
+#app {font-family: 'Avenir', Helvetica, Arial, sans-serif;-webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;color: #2c3e50;}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer">
+      <ul>
+        <li ><a href="#">Javascript</a></li>
+        <li ><a href="#">React</a></li>
+        <li ><a href="#">Java</a></li>
+        <li ><a href="#">vue</a></li>
+        <li ><a href="#">angular js</a></li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'footer'
+}
+</script>
